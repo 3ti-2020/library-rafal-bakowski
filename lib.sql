b135f8bd377f53cc3ddef2caef35fbc9d9fa25bd
Rafal001558 <62039524+Rafal001558@users.noreply.github.com>
2020-05-27 21:13:37 +0200

    Add files via upload

--- lib.sql
+++ lib.sql
@@ -1,1 +1,154 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.5
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Czas generowania: 27 Maj 2020, 21:11
+-- Wersja serwera: 10.1.38-MariaDB
+-- Wersja PHP: 7.3.2
 
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Baza danych: `library`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `autorzy`
+--
+
+CREATE TABLE `autorzy` (
+  `id_autor` int(11) NOT NULL,
+  `imie` varchar(30) COLLATE utf8_polish_ci NOT NULL,
+  `nazwisko` varchar(30) COLLATE utf8_polish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
+
+--
+-- Zrzut danych tabeli `autorzy`
+--
+
+INSERT INTO `autorzy` (`id_autor`, `imie`, `nazwisko`) VALUES
+(1, 'Rafal', 'Bakowski'),
+(2, 'Zbigniew', 'Boniek'),
+(3, 'Robert', 'Lewandowski'),
+(4, 'Lukasz', 'Piszczek'),
+(5, 'Jakub', 'Blaszczykowski');
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `ksiazki`
+--
+
+CREATE TABLE `ksiazki` (
+  `id_ksiazki` int(11) NOT NULL,
+  `id_autor` int(11) NOT NULL,
+  `id_tytul` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
+
+--
+-- Zrzut danych tabeli `ksiazki`
+--
+
+INSERT INTO `ksiazki` (`id_ksiazki`, `id_autor`, `id_tytul`) VALUES
+(1, 5, 1),
+(2, 4, 2),
+(3, 3, 3),
+(4, 2, 4),
+(5, 1, 5);
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `tytuly`
+--
+
+CREATE TABLE `tytuly` (
+  `id_tytul` int(11) NOT NULL,
+  `tytul` varchar(30) COLLATE utf8_polish_ci NOT NULL,
+  `isbn` int(3) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
+
+--
+-- Zrzut danych tabeli `tytuly`
+--
+
+INSERT INTO `tytuly` (`id_tytul`, `tytul`, `isbn`) VALUES
+(1, 'wygrac_zycie', 111),
+(2, 'java_script', 222),
+(3, 'Kurs-PHP', 333),
+(4, 'Poznaj-style-CSS', 444),
+(5, 'Przygoda-LEGO', 555);
+
+--
+-- Indeksy dla zrzutów tabel
+--
+
+--
+-- Indeksy dla tabeli `autorzy`
+--
+ALTER TABLE `autorzy`
+  ADD PRIMARY KEY (`id_autor`);
+
+--
+-- Indeksy dla tabeli `ksiazki`
+--
+ALTER TABLE `ksiazki`
+  ADD PRIMARY KEY (`id_ksiazki`),
+  ADD KEY `fk_autor` (`id_autor`),
+  ADD KEY `fk_tytul` (`id_tytul`) USING BTREE;
+
+--
+-- Indeksy dla tabeli `tytuly`
+--
+ALTER TABLE `tytuly`
+  ADD PRIMARY KEY (`id_tytul`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT dla tabeli `autorzy`
+--
+ALTER TABLE `autorzy`
+  MODIFY `id_autor` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT dla tabeli `ksiazki`
+--
+ALTER TABLE `ksiazki`
+  MODIFY `id_ksiazki` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT dla tabeli `tytuly`
+--
+ALTER TABLE `tytuly`
+  MODIFY `id_tytul` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- Ograniczenia dla zrzutów tabel
+--
+
+--
+-- Ograniczenia dla tabeli `ksiazki`
+--
+ALTER TABLE `ksiazki`
+  ADD CONSTRAINT `library_ksiazki_fk1` FOREIGN KEY (`id_autor`) REFERENCES `autorzy` (`id_autor`),
+  ADD CONSTRAINT `library_ksiazki_fk2` FOREIGN KEY (`id_tytul`) REFERENCES `tytuly` (`id_tytul`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
